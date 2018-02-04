36c45def9edef9af188b60bf7ba4978a25cd6cc8
Lunar <lunar@cypherpunk.is>
2018-02-03 21:12:11 -0600

    Update letsencrypt-to-hiawatha.sh

--- scripts/letsencrypt-to-hiawatha.sh
+++ scripts/letsencrypt-to-hiawatha.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 # Scripts that copies all new certificates over and puts them in Hiawatha format
 
-DIR1="test.xmpp.is"
-LE_DIR="/etc/letsencrypt/live/"
-HIAWATHA_CERTS="/etc/hiawatha/ssl/"
-HIAWATHA="/etc/hiawatha/"
-
-cat "${LE_DIR}${DIR1}"/privkey.pem "${LE_DIR}${DIR1}"/fullchain.pem > "${HIAWATHA_CERTS}${DIR1}".pem
+cat /etc/prosody/certs/localhost.key /etc/prosody/certs/localhost.crt > /etc/hiawatha/ssl/localhost.pem
 
 # Restart Hiawatha
 service hiawatha restart
