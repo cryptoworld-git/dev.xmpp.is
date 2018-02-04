9332c0829a6137e59e7a4ff5642ecc154cd5d6f8
Lunar <lunar@cypherpunk.is>
2018-02-03 20:26:39 -0600

    Create letsencrypt-to-prosody.sh

---
+++ scripts/letsencrypt-to-prosody.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Scripts that copies over all new certificates to Prosody and sets correct permissions
+
+DIR1="test.xmpp.is"
+LE_DIR="/etc/letsencrypt/live/"
+CERTS="/etc/prosody/certs/"
+PROSODY="/etc/prosody/"
+
+cp -rfL "${LE_DIR}${DIR1}/" "${CERTS}"
+
+chown -R prosody:prosody "${PROSODY}"
+chmod -R 700 "${CERTS}"
+prosodyctl reload
