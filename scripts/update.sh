9b969f9d579b60074a51bfd8d7065c86c5b34806
Lunar <lunar@cypherpunk.is>
2018-02-17 22:57:42 -0600

    update.sh

---
+++ scripts/update.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Script that pulls the latest files from repos and applies changes
+
+echo
+
+# Git
+cd /home/git/test.xmpp.is && git pull
+cd /home/git/mod_email_pass_reset_english && git pull
+cd /home/git/prosody_web_registration_theme && git pull
+
+# Mercurial
+cd /var/lib/prosody/modules && hg pull && hg update
+
+echo
+
+echo "Pushing new configs and files"
+
+bash /home/git/test.xmpp.is/scripts/sync.sh
+
+echo
+
+echo "Inserting Prosody secrets........."
+
+bash /root/scripts/prosody-secrets.sh
+
+echo "Forcing permissions.........."
+
+bash /home/git/test.xmpp.is/scripts/force-owner-and-group.sh
+
+echo
+
+echo "Latest configs pushed! Restart or reload services to apply changes"
+
+echo
